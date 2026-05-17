26c1158df78127bb2212a7d44fb2b03df34cd929
LianaSl <sljonkina@gmail.com>
2026-05-17 21:05:23 +0200

    update halvni

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
- export function Header() {
+export function Header() {
   return `
     <header class="header">
         <nav class="nav">
-            <a href="/" data-link>Hlavní</a>
-            <a href="/about" data-link>O mně</a>
-            <a href="/contact" data-link>Kontakt</a>
+            <ul>
+                <li><a href="/" data-link>HLAVNÍ</a></li>
+                <li><a href="/about" data-link>O MNĚ</a></li>
+                <li><a href="/certifikaty" data-link>CERTIFIKÁTY</a></li>
+                <li><a href="/cv" data-link>CV</a></li>
+                <li><a href="/fotogalerie" data-link>FOTOGALERIE</a></li>
+                <li><a href="/contact" data-link>KONTAKT</a></li>
+            </ul>
         </nav>
     </header>
     `;
 }
-
 
 
--- src/pages/Hlavni.js
+++ src/pages/Hlavni.js
@@ -1,6 +1,22 @@
 export function Hlavni() {
   return `
-  <h1>Hlavni</h1>
+  <aside class="aside">
+        <div class="parent">
+            <div class="foto">
+                <a href="#"><img class="logo-img2" src="/liana.jpg" alt="foto" fetchpriority="high"></a>
+            </div>
+            <p>Dobrý den, jmenuji se Liana.
+                Vitam Vas na sve portfolio strance.
+                Vstupuji do IT — vědomě, krok za krokem. Za sebou mám roky práce s daty, lidmi
+                a procesy. Před sebou — kód, technologie a všechno, co mě na tom opravdu baví.
+                HTML už ovládám. JavaScript a Python — učím se právě teď.
+                Není to změna profese. Konečně jdu tam, kam jsem vždycky chtěla.
+            </p>
+            <div class="foto2">
+                <a href="#"><img class="karLi" src="/karLi.jpg" alt="logo" fetchpriority="high"></a>
+            </div>
+        </div>
+    </aside>
   
   `;
 }
